ca249bb1ee4cd73d57fc18ddf00550c01eeaed37
Sumit Bopche <sumitbopche01@gmail.com>
2022-12-03 15:43:49 -0500

    high level component files

---
+++ src/components/Navigation.js
@@ -0,0 +1,5 @@
+const Navigation = () => {
+  return <div>ImProd</div>;
+};
+
+export default Navigation;
---
+++ src/pages/Main.js
@@ -0,0 +1,7 @@
+import Navigation from '../components/Navigation';
+
+const Main = () => {
+  return <Navigation />;
+};
+
+export default Main;
